20ce4968766bf83e80f4866c5c47bdf4799b7f5b
Srcka <milan.danilovic@cosmicdevelopment.com>
2017-12-22 14:41:25 +0100

    added new category

--- script.js
+++ script.js
@@ -67,7 +67,8 @@ function QuizUI(array) {
 		element.innerHTML = text;
 	},
 	this.displayScore = function() {
-		var end = "<h1>End of game</h1><br><h4>Broj tacnih odgovora je: " + array.score + "/" + array.questions.length + "</h4><br><button onclick='window.location.reload()'>Pokusaj opet</button>";
+		var end = "<h1>End of game</h1><br><h4>Broj tacnih odgovora je: " + array.score + "/" + array.questions.length +
+		"</h4><br><button onclick='window.location.reload()'>Pokusaj opet</button>";
 		this.populateHtml('question', end);
 		answersCont.style.display = "none";
 	}
@@ -75,16 +76,31 @@ function QuizUI(array) {
 
 var thrones = [
 	new Question("Simbol kuce Stark je?", ["Vuk", "Lav", "Orao", "Medved"], "Vuk"),
-	new Question("Pisac knjige Pesme leda i vatre je?", ["J. K. Rowling", "R. R. Martin", "Brandon Sanders", "J. R. R. Tolkin"], "R. R. Martin")
+	new Question("Pisac knjige Pesme leda i vatre je?", ["J. K. Rowling", "R. R. Martin", "Brandon Sanders", "J. R. R. Tolkin"], "R. R. Martin"),
+	new Question("Dzonov jezovuk se zove?", ["Nimerija", "Leto", "Sivi vetar", "Duh"], "Duh"),
+	new Question("Koliko zmajeva im Deneris", ["1", "2", "3", "4"], "3"),
+	new Question("Ko je otrovao kralja Dzofrija?", ["Rob Stark", "Lejdi Olena", "Piter  Bejlis", "Tirion Lanister"], "Lejdi Olena")
 ];
 
 var potter = [
 	new Question("U kojoj kuci Hogvortsa je bio Hari?", ["Grifindor", "Hapflpaf", "Revenklo", "Sliterin"], "Grifindor"),
-	new Question("Kako se zvala Harijeva sova?", ["Hagrid", "Hermiona", "Helga", "Hedvig"], "Hedvig")
+	new Question("Kako se zvala Harijeva sova?", ["Hagrid", "Hermiona", "Helga", "Hedvig"], "Hedvig"),
+	new Question("Kako se zvao Harijev kum?", ["Sirijus Blek", "Severus Snejp", "Vernon Darsli", "Remus Lupin"], "Sirijus Blek"),
+	new Question("Kojeg ljubimca ima Hermiona?", ["Pas", "Sova", "Macka", "Zmija"], "Macka"),
+	new Question("Koliko brace ima Ron?", ["3", "4", "5", "6"], "5")
+];
+
+var onePiece = [
+	new Question("Koliko clanova nakama ima Lufi?", ["7", "8", "9", "10"], "8"),
+	new Question("Sta je zanimanje Copera?", ["Lekar", "Zidar", "Navigator", "Arheolog"], "Lekar"),
+	new Question("Ko je Lufiju dao sesir?", ["Zoro", "Garp", "Senks", "Sabo"], "Senks"),
+	new Question("Kojoj grupi pirata je pripadala Nami?", ["Heart", "Big mama", "Red hair", "Arlong"], "Arlong"),
+	new Question("Kolika je nagrada za Usopa?", ["100 miliona", "200 miliona", "300 miliona", "400 miliona"], "200 miliona")
 ];
 
 var buttonThrones = document.getElementById("gameOfThrones");
 var buttonHarry = document.getElementById("harryPotter");
+var buttonOnePiece = document.getElementById("onePiece");
 var answersCont = document.getElementById('answersContainer');
 
 var thrones = new Quiz(thrones);
@@ -106,6 +122,15 @@ buttonHarry.onclick = function(e) {
 	answersCont.style.display = "block";
 	areasContainer.style.display = "none";
 };
+
+var piece = new Quiz(onePiece);
+
+buttonOnePiece.onclick = function (e) {
+	e.preventDefault();
+	new QuizUI(piece).display();
+	answersCont.style.display = "block";
+	areasContainer.style.display = "none";
+}
 
 
 
